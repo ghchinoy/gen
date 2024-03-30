9f0b4a3aa8ed5b2aebd7463ba553df6b8550874d
Hussain Chinoy <ghchinoy@google.com>
2024-03-30 13:58:18 -0600

    tokens count tokens command

---
+++ cmd/tokens.go
@@ -0,0 +1,62 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+
+	"cloud.google.com/go/vertexai/genai"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(tokensCmd)
+
+	tokensCmd.PersistentFlags().StringVarP(&modelName, "model", "m", "gemini-1.0-pro", "model name")
+}
+
+var tokensCmd = &cobra.Command{
+	Use:     "tokens",
+	Aliases: []string{"t", "count", "tokencount", "tc"},
+	Short:   "count tokens for prompt",
+	Long:    `Returns the count of tokens for a provided prompt`,
+	Run:     countTokensForPrompt,
+}
+
+// countTokensForPrompt is the cobra implementation of countTokens
+func countTokensForPrompt(cmd *cobra.Command, args []string) {
+	if len(args) == 0 {
+		log.Fatal("requires a prompt to count tokens")
+	}
+	prompt := args[0]
+	err := countTokens(os.Stdout, string(prompt), projectID, region, modelName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+// countTokens returns the number of tokens for this prompt.
+func countTokens(w io.Writer, prompt, projectID, location, modelName string) error {
+	ctx := context.Background()
+
+	client, err := genai.NewClient(ctx, projectID, location)
+	if err != nil {
+		return fmt.Errorf("unable to create client: %v", err)
+	}
+	defer client.Close()
+
+	model := client.GenerativeModel(modelName)
+
+	resp, err := model.CountTokens(ctx, genai.Text(prompt))
+	if err != nil {
+		return err
+	}
+
+	fmt.Fprintf(w, "Number of tokens for the prompt: %s\n", strconv.FormatInt(int64(resp.TotalTokens), 10))
+
+	return nil
+}
