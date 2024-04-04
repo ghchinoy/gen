93c22494a4c296e581e3f4d0d3db2203b32dd989
ghchinoy <ghchinoy@gmail.com>
2024-04-03 22:33:06 -0600

    feat: adds claude rawpredict #2

--- cmd/prompt.go
+++ cmd/prompt.go
@@ -15,6 +15,7 @@ import (
 	"cloud.google.com/go/vertexai/genai"
 	"github.com/spf13/cobra"
 	"google.golang.org/api/option"
+	"google.golang.org/genproto/googleapis/api/httpbody"
 	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/types/known/structpb"
 )
@@ -141,7 +142,7 @@ func useClaudeModel(projectID string, region string, modelName string, args []st
 		log.Printf("error generating content: %v", err)
 		os.Exit(1)
 	}
-	log.Printf("generated content: %s", buf.String())
+	fmt.Printf("%s\n", buf.String())
 	return nil
 }
 
@@ -283,73 +284,57 @@ func generateContentClaude(w io.Writer, prompt, projectID, location, publisher, 
 	// Endpoint
 	base := fmt.Sprintf("projects/%s/locations/%s/publishers/%s/models", projectID, location, publisher)
 	url := fmt.Sprintf("%s/%s", base, model)
-	log.Printf("url: %s", url)
+	if logtype != "none" {
+		log.Printf("url: %s", url)
+	}
 
 	// Construct an Anthropic message.
-	// []messages{ role, []content { type, text }, max_tokens }
-	message, err := structpb.NewValue(map[string]interface{}{
-		"role": "user",
-		"content": []interface{}{
-			map[string]interface{}{
-				"type": "text",
-				"text": prompt,
-			}},
-	})
+	claudeRequest := AnthropicRequest{
+		AnthropicVersion: "vertex-2023-10-16",
+		MaxTokens:        256,
+		Stream:           false,
+		Messages: []AnthropicMessage{
+			{
+				Content: []AnthropicContent{
+					{
+						Text: prompt,
+						Type: "text",
+					},
+				},
+				Role: "user",
+			},
+		},
+	}
+
+	data, err := json.Marshal(&claudeRequest)
 	if err != nil {
-		log.Printf("can't create message from content: %v", err)
-		return err
-	}
-	log.Printf("message: %v", message.GetStructValue().AsMap())
-
-	messages, err := structpb.NewList([]interface{}{
-		message.GetStructValue().AsMap(),
-	})
-	if err != nil {
-		log.Printf("can't create messages from message: %v", err)
-		return err
-	}
-	log.Printf("messages: %v", messages)
-
-	// Instances: the prompt to use with the text model
-	promptValue, err := structpb.NewValue(map[string]interface{}{
-		"anthropic_version": "vertex-2023-10-16",
-		"max_tokens":        parameters["maxTokens"].(int),
-		"stream":            false,
-		"messages":          messages.AsSlice(),
-	})
-	if err != nil {
-		log.Printf("can't create promptValue: %v", err)
-		return err
-	}
-	jsonbytes, _ := protojson.Marshal(promptValue)
-	log.Printf("jsonbytes: %s", jsonbytes)
-
-	/*
-		messagesValue, err := structpb.NewValue(map[string]interface{}{
-			"messages": messages.AsSlice(),
-		})
-		if err != nil {
-			log.Printf("can't create messagesValue: %v", err)
-
-			return err
-		}
-	*/
-
-	// PredictRequest: create the model prediction request
-	req := &aiplatformpb.PredictRequest{
-		Endpoint:  url,
-		Instances: []*structpb.Value{message},
-		//Parameters: parametersValue,
+		return fmt.Errorf("error marshalling ClaudeRequest: %v", err)
 	}
 
-	// PredictResponse: receive the response from the model
-	resp, err := client.Predict(ctx, req)
+	// using RawPredict
+	req := &aiplatformpb.RawPredictRequest{
+		Endpoint: url,
+		HttpBody: &httpbody.HttpBody{
+			ContentType: "application/json",
+			Data:        data,
+		},
+	}
+
+	resp, err := client.RawPredict(ctx, req)
 	if err != nil {
 		fmt.Fprintf(w, "error in prediction: %v", err)
 		return err
 	}
 
-	fmt.Fprintf(w, "text-prediction response: %v", resp.Predictions[0])
+	if outputtype == "json" {
+		fmt.Fprintln(w, string(resp.Data))
+	} else {
+		var r AnthropicResponse
+		_ = json.Unmarshal(resp.Data, &r)
+		fmt.Fprintf(w, "%v", r.Content[0].Text)
+
+	}
+
 	return nil
 }
 
@@ -369,6 +354,47 @@ func readModelConfigFile(configFile string) (map[string]interface{}, error) {
 	return config, nil
 }
 
+// AnthropicRequest is the request to the Claude model.
+type AnthropicRequest struct {
+	AnthropicVersion string             `json:"anthropic_version,omitempty"`
+	MaxTokens        int                `json:"max_tokens,omitempty"`
+	Messages         []AnthropicMessage `json:"messages,omitempty"`
+	Stream           bool               `json:"stream,omitempty"`
+	TopP             float32            `json:"top_p,omitempty"`
+	TopK             int                `json:"top_k,omitempty"`
+	Temperature      float32            `json:"temperature,omitempty"`
+}
+
+// AnthropicMessage is a message to the Claude model.
+type AnthropicMessage struct {
+	Content []AnthropicContent `json:"content,omitempty"`
+	Role    string             `json:"role,omitempty"`
+}
+
+// AnthropicContent is the content of a message to the Claude model.
+type AnthropicContent struct {
+	Text string `json:"text,omitempty"`
+	Type string `json:"type,omitempty"`
+}
+
+// AnthropicResponse is the response from the Claude model.
+type AnthropicResponse struct {
+	ID           string             `json:"id,omitempty"`
+	Type         string             `json:"type,omitempty"`
+	Role         string             `json:"role,omitempty"`
+	Content      []AnthropicContent `json:"content,omitempty"`
+	Model        string             `json:"model,omitempty"`
+	StopReason   string             `json:"stop_reason,omitempty"`
+	StopSequence string             `json:"stop_sequence,omitempty"`
+	Usage        AnthropicUsage     `json:"usage,omitempty"`
+}
+
+type AnthropicUsage struct {
+	InputTokens  int `json:"input_tokens,omitempty"`
+	OutputTokens int `json:"output_tokens,omitempty"`
+}
+
+// PaLMResponse is the response from the PaLM model.
 type PaLMResponse struct {
 	Predictions []Prediction `json:"predictions"`
 	Metadata    Metadata     `json:"metadata"`
