bb5575c8989efc23a9f8ee2006f1f89add050ab1
Bruce Maxfield <bmaxfield@google.com>
2024-04-12 09:50:44 -0700

    Remove dependency on models txt file

--- cmd/models.go
+++ cmd/models.go
@@ -9,9 +9,6 @@ import (
 	"github.com/ghchinoy/gen/internal/model"
 )
 
-//go:embed models
-var modellist string
-
 func init() {
 	rootCmd.AddCommand(modelsCmd)
 }
@@ -25,39 +22,13 @@ var modelsCmd = &cobra.Command{
 	Hidden:  true,
 }
 
-// TODO - Need to refactor this to use the tablewriter package
 func listModels(cmd *cobra.Command, args []string) {
-	// read modellist string via CSV reader
-	// r := csv.NewReader(strings.NewReader(modellist))
-	// modelarray, err := r.ReadAll()
-	// if err != nil {
-	// 	log.Printf("unable to read model list: %v", err)
-	// 	os.Exit(1)
-	// }
 
 	mdls := model.List()
 
-	// fmt.Println("Family:\t	Type:\t	Name:")
-
-	// data := [][]string{}
 	for _, v := range mdls {
 
 		fmt.Println(v)
-
-		// if strings.HasPrefix(v[0], "#") {
-		// 	continue
-		// }
-		// data = append(data, []string{
-		// 	v[0], // group
-		// 	v[1], // type
-		// 	v[2], // name
-		// })
 	}
-	// table := tablewriter.NewWriter(os.Stdout)
-	// table.SetHeader([]string{"Family", "Type", "Name"})
-	// table.SetBorder(false)
-	// table.SetAlignment(tablewriter.ALIGN_LEFT)
-	// table.AppendBulk(data)
-	// table.Render()
 
 }
