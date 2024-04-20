67e640e1a2f3842626a737cf223f050dc1f21e4f
Hussain Chinoy <ghchinoy@google.com>
2024-04-20 14:05:52 -0600

    chore: updates to stable gemini model

--- cmd/tokens.go
+++ cmd/tokens.go
@@ -17,7 +17,7 @@ var promptFile string
 func init() {
 	rootCmd.AddCommand(tokensCmd)
 
-	tokensCmd.PersistentFlags().StringVarP(&modelName, "model", "m", "gemini-1.0-pro", "model name")
+	tokensCmd.PersistentFlags().StringVarP(&modelName, "model", "m", "gemini-1.0-pro-001", "model name")
 	tokensCmd.PersistentFlags().StringVarP(&promptFile, "file", "f", "", "prompt file")
 }
 
